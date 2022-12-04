a8d4ab5765282b458284ab8b8b37541f7ad574eb
unknown <patriciaaquino100@hotmail.com>
2022-12-04 14:35:19 -0300

    agregar archivos al repo

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function login(){
-    var  user, pass;
+    var  user, pass, error;
     
     user = document.getElementById("usuario").value;
     pass = document.getElementById("contrasena").value;
-
+    error = document.getElementById("Contrasena o usuario equivocado").value;
 
     if((user == "beto") && (pass == "1234")){
         
-        window.location = "MiCuenta.html";
-
-    }
+       window.location = "miCuenta.html";
+    }{error
+}
 }
